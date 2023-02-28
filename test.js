b0b4353fed0d7d4a3e8a77b6d2ffb9f430b934dc
Roman91A <106251537+Roman91A@users.noreply.github.com>
2023-02-28 15:05:22 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,23 @@
+var startTime = '28.02.2023 14:48'
+var endTime = '10.03.2023 00:00'
+var currentDate = moment.utc()
+
+function BetweenDates() {
+    var start = moment.utc(startTime + " +0300", "DD.MM.YYYY HH:mm Z");
+    var end = moment.utc(endTime + " +0300", "DD.MM.YYYY HH:mm Z");
+    return currentDate.isBetween(start, end)
+}
+console.log(BetweenDates())
+
+
+
+var startTime = '28.02.2023 14:50'
+var endTime = '10.03.2023 00:00'
+var currentDate = moment.utc().add(3, 'hours');
+
+function BetweenDates1() {
+    var start = moment.utc(startTime, "DD.MM.YYYY HH:mm");
+    var end = moment.utc(endTime, "DD.MM.YYYY HH:mm");
+    return currentDate.isBetween(start, end)
+}
+console.log(BetweenDates1())
