08150c0cb50d50dc37a17d9b51f058abbc8c99e0
Roman91A <106251537+Roman91A@users.noreply.github.com>
2023-02-28 19:16:22 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
-// Пример 1 
+// Имеется 2 функции BetweenDates и BetweenDates1, на вход принимаю 3 аргумента (Начала теста, Конец теста, текущее время). 
+// Если currentDate входит в (start, end) то возвращает true, иначе false 
+
+
+
 var startTime = '28.02.2023 14:48'
 var endTime = '10.03.2023 00:00'
+
+
+
+// Пример 1 (startTime, endTime приводим к UTC, после у currentDate вызываем isBetween)
 var currentDate = moment.utc()
 
 function BetweenDates() {
@@ -11,11 +19,8 @@ function BetweenDates() {
 console.log(BetweenDates())
 
 
-// Пример 2
-var startTime = '28.02.2023 14:50'
-var endTime = '10.03.2023 00:00'
+// Пример 2 (currentDate смещаем на 3 часа, в результате currentDate = время МСК, после у currentDate вызываем isBetween)
 var currentDate = moment.utc().add(3, 'hours');
-
 function BetweenDates1() {
     var start = moment.utc(startTime, "DD.MM.YYYY HH:mm");
     var end = moment.utc(endTime, "DD.MM.YYYY HH:mm");
